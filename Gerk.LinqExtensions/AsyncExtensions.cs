dc2504a49de3c1088793c2141123658ed966a126
Benji Altman <benji@altmansoftwaredesign.com>
2021-08-26 10:34:05 -0500

    Added ForEachAsync

--- Gerk.LinqExtensions/AsyncExtensions.cs
+++ Gerk.LinqExtensions/AsyncExtensions.cs
@@ -48,5 +48,41 @@ namespace Gerk.LinqExtensions
 			await Task.WhenAll(tasks);
 			return output;
 		}
+
+		/// <summary>
+		/// Runs an asynchronous function on each element of an enumerable. Execution order is not gaurenteed. This method does not support defered execution and will force execution on <paramref name="self"/>.
+		/// </summary>
+		/// <typeparam name="In">The input members (elements of <paramref name="self"/>)</typeparam>
+		/// <param name="self">Enumerable that we are starting with.</param>
+		/// <param name="action"><see langword="async"/> function that will act on each element of <paramref name="self"/></param>
+		/// <param name="concurrencyLimit">Maximum tasks to have running in parallel. A <c>0</c> will have no limit.</param>
+		/// <returns></returns>
+		public static async Task ForEachAsync<In>(this IEnumerable<In> self, Func<In, Task> action, int concurrencyLimit = 0)
+        {
+			if (concurrencyLimit <= 0)
+			{
+				await Task.WhenAll(self.Select(action));
+			}
+			else
+			{
+				var inputList = self.ToList();
+				var tasks = new HashSet<Task>();
+
+				int i = 0;
+
+				for (; i < concurrencyLimit && i < inputList.Count; i++)
+				{
+					tasks.Add(action(inputList[i]));
+				}
+
+				for (; i < inputList.Count; i++)
+				{
+					tasks.Remove(await Task.WhenAny(tasks));
+					tasks.Add(action(inputList[i]));
+				}
+
+				await Task.WhenAll(tasks);
+			}
+		}
 	}
 }
