68309ed7ea9eafa65935664498007bef4e590018
Benji Altman <benji@altmansoftwaredesign.com>
2021-10-11 22:24:58 -0500

    ForeachAsync now has cancellation token version

--- Gerk.LinqExtensions/AsyncExtensions.cs
+++ Gerk.LinqExtensions/AsyncExtensions.cs
@@ -30,7 +30,7 @@ namespace Gerk.LinqExtensions
 		/// <typeparam name="Out">The output type</typeparam>
 		/// <param name="self">List that we are starting with.</param>
 		/// <param name="func"><see langword="async"/> function that maps from elements of <paramref name="self"/> to an output type.</param>
-		/// <param name="cancellationToken">Cancellation token that will be passed to each call of <paramref name="func"/>. Triggering it will not immidately end execution of the function, however its calling will be passed on to each call.</param>
+		/// <param name="cancellationToken">Cancellation token that will be passed to each call of <paramref name="func"/>. Triggering it will not immidately end execution of the function.</param>
 		/// <param name="concurrencyLimit">Maximum tasks to have running in parallel. A <c>0</c> will have no limit.</param>
 		/// <returns></returns>
 		public static async Task<Out[]> SelectAsync<In, Out>(this IEnumerable<In> self, Func<In, CancellationToken, Task<Out>> func, CancellationToken cancellationToken, int concurrencyLimit = 0)
@@ -70,11 +70,22 @@ namespace Gerk.LinqExtensions
 		/// <param name="action"><see langword="async"/> function that will act on each element of <paramref name="self"/></param>
 		/// <param name="concurrencyLimit">Maximum tasks to have running in parallel. A <c>0</c> will have no limit.</param>
 		/// <returns></returns>
-		public static async Task ForEachAsync<In>(this IEnumerable<In> self, Func<In, Task> action, int concurrencyLimit = 0)
+		public static Task ForEachAsync<In>(this IEnumerable<In> self, Func<In, Task> action, int concurrencyLimit = 0) => self.ForEachAsync((x, _) => action(x), CancellationToken.None, concurrencyLimit);
+
+		/// <summary>
+		/// Runs an asynchronous function on each element of an enumerable. Execution order is not gaurenteed. This method does not support defered execution and will force execution on <paramref name="self"/>.
+		/// </summary>
+		/// <typeparam name="In">The input members (elements of <paramref name="self"/>)</typeparam>
+		/// <param name="self">Enumerable that we are starting with.</param>
+		/// <param name="action"><see langword="async"/> function that will act on each element of <paramref name="self"/></param>
+		/// <param name="cancellationToken">Cancellation token that will be passed to each call of <paramref name="action"/>. Triggering it will not immidately end execution of the function.</param>
+		/// <param name="concurrencyLimit">Maximum tasks to have running in parallel. A <c>0</c> will have no limit.</param>
+		/// <returns></returns>
+		public static async Task ForEachAsync<In>(this IEnumerable<In> self, Func<In, CancellationToken, Task> action, CancellationToken cancellationToken, int concurrencyLimit = 0)
 		{
 			if (concurrencyLimit <= 0)
 			{
-				await Task.WhenAll(self.Select(action));
+				await Task.WhenAll(self.Select(x => action(x, cancellationToken)));
 			}
 			else
 			{
@@ -85,13 +96,13 @@ namespace Gerk.LinqExtensions
 
 				for (; i < concurrencyLimit && i < inputList.Count; i++)
 				{
-					tasks.Add(action(inputList[i]));
+					tasks.Add(action(inputList[i], cancellationToken));
 				}
 
 				for (; i < inputList.Count; i++)
 				{
 					tasks.Remove(await Task.WhenAny(tasks));
-					tasks.Add(action(inputList[i]));
+					tasks.Add(action(inputList[i], cancellationToken));
 				}
 
 				await Task.WhenAll(tasks);
