22e702a9398db6bd65e99030f2f629c65146dee1
Benji Altman <benji@altmansoftwaredesign.com>
2021-10-13 11:50:05 -0500

    Add FindMatch, needs documentation still

--- Gerk.LinqExtensions/AsyncExtensions.cs
+++ Gerk.LinqExtensions/AsyncExtensions.cs
@@ -12,6 +12,7 @@ namespace Gerk.LinqExtensions
 	/// </summary>
 	public static class AsyncExtensions
 	{
+		#region SelectAsync
 		/// <summary>
 		/// Runs an asynchronous function on each element of an enumerable. Ordering is maintained from input to output, but internal execution order is not gaurenteed to match this. This method does not support defered execution and will force execution on <paramref name="self"/>.
 		/// </summary>
@@ -19,9 +20,8 @@ namespace Gerk.LinqExtensions
 		/// <typeparam name="Out">The output type</typeparam>
 		/// <param name="self">List that we are starting with.</param>
 		/// <param name="func"><see langword="async"/> function that maps from elements of <paramref name="self"/> to an output type.</param>
-		/// <param name="concurrencyLimit">Maximum tasks to have running in parallel. A <c>0</c> will have no limit.</param>
 		/// <returns></returns>
-		public static Task<Out[]> SelectAsync<In, Out>(this IEnumerable<In> self, Func<In, Task<Out>> func, int concurrencyLimit = 0) => self.SelectAsync((x, _) => func(x), CancellationToken.None, concurrencyLimit);
+		public static Task<Out[]> SelectAsync<In, Out>(this IEnumerable<In> self, Func<In, Task<Out>> func) => self.SelectAsync((x, _) => func(x), CancellationToken.None);
 
 		/// <summary>
 		/// Runs an asynchronous function on each element of an enumerable. Ordering is maintained from input to output, but internal execution order is not gaurenteed to match this. This method does not support defered execution and will force execution on <paramref name="self"/>.
@@ -30,20 +30,39 @@ namespace Gerk.LinqExtensions
 		/// <typeparam name="Out">The output type</typeparam>
 		/// <param name="self">List that we are starting with.</param>
 		/// <param name="func"><see langword="async"/> function that maps from elements of <paramref name="self"/> to an output type.</param>
-		/// <param name="cancellationToken">Cancellation token that will be passed to each call of <paramref name="func"/>. Triggering it will not immidately end execution of the function.</param>
-		/// <param name="concurrencyLimit">Maximum tasks to have running in parallel. A <c>0</c> will have no limit.</param>
+		/// <param name="concurrencyLimit">Maximum tasks to have running in parallel.</param>
 		/// <returns></returns>
-		public static async Task<Out[]> SelectAsync<In, Out>(this IEnumerable<In> self, Func<In, CancellationToken, Task<Out>> func, CancellationToken cancellationToken, int concurrencyLimit = 0)
+		public static Task<Out[]> SelectAsync<In, Out>(this IEnumerable<In> self, Func<In, Task<Out>> func, int concurrencyLimit) => self.SelectAsync((x, _) => func(x), CancellationToken.None, concurrencyLimit);
+
+		/// <summary>
+		/// Runs an asynchronous function on each element of an enumerable. Ordering is maintained from input to output, but internal execution order is not gaurenteed to match this. This method does not support defered execution and will force execution on <paramref name="self"/>.
+		/// </summary>
+		/// <typeparam name="In">The input members (elements of <paramref name="self"/>)</typeparam>
+		/// <typeparam name="Out">The output type</typeparam>
+		/// <param name="self">List that we are starting with.</param>
+		/// <param name="func"><see langword="async"/> function that maps from elements of <paramref name="self"/> to an output type.</param>
+		/// <param name="cancellationToken">Cancellation token that will be passed to each call of <paramref name="func"/></param>
+		/// <returns></returns>
+		public static async Task<Out[]> SelectAsync<In, Out>(this IEnumerable<In> self, Func<In, CancellationToken, Task<Out>> func, CancellationToken cancellationToken) => await Task.WhenAll(self.Select(x => func(x, cancellationToken)));
+
+		/// <summary>
+		/// Runs an asynchronous function on each element of an enumerable. Ordering is maintained from input to output, but internal execution order is not gaurenteed to match this. This method does not support defered execution and will force execution on <paramref name="self"/>.
+		/// </summary>
+		/// <typeparam name="In">The input members (elements of <paramref name="self"/>)</typeparam>
+		/// <typeparam name="Out">The output type</typeparam>
+		/// <param name="self">List that we are starting with.</param>
+		/// <param name="func"><see langword="async"/> function that maps from elements of <paramref name="self"/> to an output type.</param>
+		/// <param name="cancellationToken">Cancellation token that will be passed to each call of <paramref name="func"/></param>
+		/// <param name="concurrencyLimit">Maximum tasks to have running in parallel.</param>
+		/// <returns></returns>
+		public static async Task<Out[]> SelectAsync<In, Out>(this IEnumerable<In> self, Func<In, CancellationToken, Task<Out>> func, CancellationToken cancellationToken, int concurrencyLimit)
 		{
-			if (concurrencyLimit <= 0)
-				return await Task.WhenAll(self.Select(x => func(x, cancellationToken)));
-
 			var inputList = self.ToList();
 			Out[] output = new Out[inputList.Count];
 			var tasks = new HashSet<Task>();
 
 			// Run the function on the idx'th element from the input and then assign it into the output. This is all ecapsulated within a task that is put into our pool of tasks.
-			Action<int> startExecution = idx => tasks.Add(func(inputList[idx], cancellationToken).Then(o => output[idx] = o));
+			void startExecution(int idx) => tasks.Add(func(inputList[idx], cancellationToken).Then(o => output[idx] = o));
 
 			int i = 0;
 
@@ -61,6 +80,17 @@ namespace Gerk.LinqExtensions
 			await Task.WhenAll(tasks);
 			return output;
 		}
+		#endregion
+
+		#region ForEach
+		/// <summary>
+		/// Runs an asynchronous function on each element of an enumerable. Execution order is not gaurenteed. This method does not support defered execution and will force execution on <paramref name="self"/>.
+		/// </summary>
+		/// <typeparam name="In">The input members (elements of <paramref name="self"/>)</typeparam>
+		/// <param name="self">Enumerable that we are starting with.</param>
+		/// <param name="action"><see langword="async"/> function that will act on each element of <paramref name="self"/></param>
+		/// <returns></returns>
+		public static Task ForEachAsync<In>(this IEnumerable<In> self, Func<In, Task> action) => self.ForEachAsync((x, _) => action(x), CancellationToken.None);
 
 		/// <summary>
 		/// Runs an asynchronous function on each element of an enumerable. Execution order is not gaurenteed. This method does not support defered execution and will force execution on <paramref name="self"/>.
@@ -68,9 +98,9 @@ namespace Gerk.LinqExtensions
 		/// <typeparam name="In">The input members (elements of <paramref name="self"/>)</typeparam>
 		/// <param name="self">Enumerable that we are starting with.</param>
 		/// <param name="action"><see langword="async"/> function that will act on each element of <paramref name="self"/></param>
-		/// <param name="concurrencyLimit">Maximum tasks to have running in parallel. A <c>0</c> will have no limit.</param>
+		/// <param name="concurrencyLimit">Maximum tasks to have running in parallel.</param>
 		/// <returns></returns>
-		public static Task ForEachAsync<In>(this IEnumerable<In> self, Func<In, Task> action, int concurrencyLimit = 0) => self.ForEachAsync((x, _) => action(x), CancellationToken.None, concurrencyLimit);
+		public static Task ForEachAsync<In>(this IEnumerable<In> self, Func<In, Task> action, int concurrencyLimit) => self.ForEachAsync((x, _) => action(x), CancellationToken.None, concurrencyLimit);
 
 		/// <summary>
 		/// Runs an asynchronous function on each element of an enumerable. Execution order is not gaurenteed. This method does not support defered execution and will force execution on <paramref name="self"/>.
@@ -78,35 +108,142 @@ namespace Gerk.LinqExtensions
 		/// <typeparam name="In">The input members (elements of <paramref name="self"/>)</typeparam>
 		/// <param name="self">Enumerable that we are starting with.</param>
 		/// <param name="action"><see langword="async"/> function that will act on each element of <paramref name="self"/></param>
-		/// <param name="cancellationToken">Cancellation token that will be passed to each call of <paramref name="action"/>. Triggering it will not immidately end execution of the function.</param>
-		/// <param name="concurrencyLimit">Maximum tasks to have running in parallel. A <c>0</c> will have no limit.</param>
+		/// <param name="cancellationToken">Cancellation token that will be passed to each call of <paramref name="action"/></param>
 		/// <returns></returns>
-		public static async Task ForEachAsync<In>(this IEnumerable<In> self, Func<In, CancellationToken, Task> action, CancellationToken cancellationToken, int concurrencyLimit = 0)
+		public static async Task ForEachAsync<In>(this IEnumerable<In> self, Func<In, CancellationToken, Task> action, CancellationToken cancellationToken) => await Task.WhenAll(self.Select(x => action(x, cancellationToken)));
+
+		/// <summary>
+		/// Runs an asynchronous function on each element of an enumerable. Execution order is not gaurenteed. This method does not support defered execution and will force execution on <paramref name="self"/>.
+		/// </summary>
+		/// <typeparam name="In">The input members (elements of <paramref name="self"/>)</typeparam>
+		/// <param name="self">Enumerable that we are starting with.</param>
+		/// <param name="action"><see langword="async"/> function that will act on each element of <paramref name="self"/></param>
+		/// <param name="cancellationToken">Cancellation token that will be passed to each call of <paramref name="action"/></param>
+		/// <param name="concurrencyLimit">Maximum tasks to have running in parallel.</param>
+		/// <returns></returns>
+		public static async Task ForEachAsync<In>(this IEnumerable<In> self, Func<In, CancellationToken, Task> action, CancellationToken cancellationToken, int concurrencyLimit)
 		{
-			if (concurrencyLimit <= 0)
+			var inputList = self.ToList();
+			var tasks = new HashSet<Task>();
+
+			int i = 0;
+
+			for (; i < concurrencyLimit && i < inputList.Count; i++)
 			{
-				await Task.WhenAll(self.Select(x => action(x, cancellationToken)));
+				tasks.Add(action(inputList[i], cancellationToken));
 			}
-			else
+
+			for (; i < inputList.Count; i++)
 			{
-				var inputList = self.ToList();
-				var tasks = new HashSet<Task>();
+				tasks.Remove(await Task.WhenAny(tasks));
+				tasks.Add(action(inputList[i], cancellationToken));
+			}
 
-				int i = 0;
+			await Task.WhenAll(tasks);
+		}
+		#endregion
 
-				for (; i < concurrencyLimit && i < inputList.Count; i++)
+		#region FindMatch
+		private static async Task<(In Value, bool Found)> FindMatchAsyncHelper<In>(Func<Func<In, Task>, Task> forEach, Func<In, CancellationToken, Task<bool>> predicate, CancellationToken cancellationToken)
+		{
+			using (CancellationTokenSource cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken))
+			{
+				In output = default;
+				bool found = false;
+				try
 				{
-					tasks.Add(action(inputList[i], cancellationToken));
+					await forEach(async item =>
+					{
+						if (await predicate(item, cts.Token))
+						{
+							output = item;
+							found = true;
+							cts.Cancel();
+						}
+					});
 				}
-
-				for (; i < inputList.Count; i++)
+				catch (TaskCanceledException)
 				{
-					tasks.Remove(await Task.WhenAny(tasks));
-					tasks.Add(action(inputList[i], cancellationToken));
+					if (found)
+						return (output, true);
+					else
+						throw;
 				}
-
-				await Task.WhenAll(tasks);
+				return (output, found);
 			}
 		}
+
+		public static Task<(In Value, bool Found)> FindMatchAsync<In>(this IEnumerable<In> self, Func<In, Task<bool>> predicate)
+		{
+			var tcs = new TaskCompletionSource<(In, bool)>();
+			var tasks = new List<Task>();
+			foreach (var elem in self)
+			{
+				var e = elem;
+				tasks.Add(predicate(elem).Then(x =>
+				{
+					if (x)
+					{
+						tcs.TrySetResult((e, true));
+					}
+				}));
+
+				if (tcs.Task.IsCompleted)
+					break;
+			}
+
+			Task.WhenAll(tasks).Then(() =>
+			{
+				tcs.TrySetResult((default, false));
+			});
+
+			return tcs.Task;
+		}
+
+		public static async Task<(In Value, bool Found)> FindMatchAsync<In>(this IEnumerable<In> self, Func<In, Task<bool>> predicate, int concurrencyLimit)
+		{
+			var tasks = new HashSet<Task<(In Value, bool Found)>>(concurrencyLimit);
+			void startNextTask(In elem) => tasks.Add(predicate(elem).Then(x => (elem, x)));
+			async Task<(In Value, bool Found)> completeATask()
+			{
+				var t = await Task.WhenAny(tasks);
+				var response = await t;
+
+				tasks.Remove(t);
+
+				return response;
+			}
+
+			var enumerator = self.GetEnumerator();
+			for (int i = 0; i < concurrencyLimit && enumerator.MoveNext(); i++)
+				startNextTask(enumerator.Current);
+
+			while (enumerator.MoveNext())
+			{
+				var response = await completeATask();
+				if (response.Found)
+					return response;
+
+				startNextTask(enumerator.Current);
+			}
+
+			while (tasks.Count != 0)
+			{
+				var response = await completeATask();
+				if (response.Found)
+					return response;
+			}
+
+			return (default, false);
+		}
+
+		public static Task<(In Value, bool Found)> FindMatchAsync<In>(this IEnumerable<In> self, Func<In, CancellationToken, Task<bool>> predicate) => self.FindMatchAsync(predicate, CancellationToken.None);
+
+		public static Task<(In Value, bool Found)> FindMatchAsync<In>(this IEnumerable<In> self, Func<In, CancellationToken, Task<bool>> predicate, int concurrencyLimit) => self.FindMatchAsync(predicate, CancellationToken.None, concurrencyLimit);
+
+		public static Task<(In Value, bool Found)> FindMatchAsync<In>(this IEnumerable<In> self, Func<In, CancellationToken, Task<bool>> predicate, CancellationToken cancellationToken) => FindMatchAsyncHelper(f => self.ForEachAsync(f), predicate, cancellationToken);
+
+		public static Task<(In Value, bool Found)> FindMatchAsync<In>(this IEnumerable<In> self, Func<In, CancellationToken, Task<bool>> predicate, CancellationToken cancellationToken, int concurrencyLimit) => FindMatchAsyncHelper(f => self.ForEachAsync(f, concurrencyLimit), predicate, cancellationToken);
+		#endregion
 	}
 }
